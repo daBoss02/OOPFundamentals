03e7bc30909223c7c5a59ed65b92c71dcada761f
Seth Vandenbos <seth.vdbos@gmail.com>
2023-01-25 15:42:59 -0600

    Product class donee

---
+++ Lab1/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Product
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length > 1)
+                {
+                    _name = value;
+                } else
+                {
+                    throw new Exception("Length must be greater than 1");
+                }
+            }
+        }
+        private int _price;
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+            }
+        }
+        private string _code;
+         public Product(string code)
+        {
+            _code = code;
+        }
+    }
+}
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+A vending machine has a set of coins to handle purchase operations.
+When a customer buys a product from the machine and pays, the machine
+will return the remaining money from the coins in its internal storage.
+Suppose our machine has the following coins:
+$1, $2, $5, $10, $20
+The machine tracks the quantity of each coin: for example, the quantity of $5 coins is 15,
+which means the machine currently has 15 pieces of $5 coins.
+*/
+
+
+using Lab1;
+
+Dictionary<int, int> coins = new Dictionary<int, int>()
+{
+    {1, 10},
+    {2, 10},
+    {5, 5},
+    {10, 5},
+    {20, 5}
+};
+VendingMachine vendor = new VendingMachine(coins);
+vendor.StockItem();
+vendor.StockItem();
+vendor.StockItem();
+vendor.StockItem();
+
+vendor.printInv();
