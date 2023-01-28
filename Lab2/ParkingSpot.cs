eec343aee180638618ef6627df09bb6121dd6c18
Seth Vandenbos <seth.vdbos@gmail.com>
2023-01-27 19:28:40 -0600

    working carpark

---
+++ Lab2/ParkingSpot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class ParkingSpot
+    {
+        private int _number;
+        private Vehicle? _vehicle;
+        private CarPark _carPark;
+
+        public int Number { get { return _number; } }
+        public CarPark CarPark { get { return _carPark; } }
+
+        public void SetCarPark(CarPark carpark)
+        {
+            _carPark = carpark;
+        }
+        public Vehicle? Vehicle
+        {
+            get { return _vehicle; }
+            set
+            {
+                _vehicle = value;
+            }
+        }
+
+        public ParkingSpot(int number, CarPark carpark)
+        {
+            _number = number;
+            _carPark = carpark;
+        }
+
+        public ParkingSpot(int number)
+        {
+            _number = number;
+        }
+    }
+}
---
+++ Lab2/Vehicle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Vehicle
+    {
+        private string? _licenseNumber = "0000000";
+        public string? LicenseNumber
+        {
+            get { return _licenseNumber; }
+            set
+            {
+                if (value.Length < 3 || value.Length > 7 || value.Contains(" "))
+                {
+                    throw new Exception("Plate must be alphanumeric between 3 and 7 characters");
+                }
+                else
+                {
+                    _licenseNumber = value;
+                }
+            }
+        }
+
+        private void UpdateLicenseNumber()
+        {
+
+        }
+
+        private HashSet<ParkingSpot> _parkingSpots = new HashSet<ParkingSpot>();
+
+        public HashSet<ParkingSpot> ParkingSpots { get { return _parkingSpots; } }
+        public void RemoveParkingSpot(CarPark carpark)
+        {
+            foreach (ParkingSpot spot in _parkingSpots)
+            {
+                if (spot.CarPark == carpark)
+                {
+                    _parkingSpots.Remove(spot);
+                }
+            }
+        }
+
+        public void addSpot(ParkingSpot parkingSpot)
+        {
+            _parkingSpots.Add(parkingSpot);
+        }
+
+        public Vehicle(string licenseNumber)
+        {
+            LicenseNumber = licenseNumber;
+        }
+        public Vehicle()
+        {
+
+        }
+
+    }
+}
