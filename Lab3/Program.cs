15f019d79a6578ae6355cffc043527c33db043e0
Seth Vandenbos <seth.vdbos@gmail.com>
2023-01-30 13:31:54 -0600

    added necessary methods and classes

---
+++ Lab3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Create a system in which Students can register for Courses
+// A student may only take one Course, where a Course can have many students
+
+// Student Class
+// Course Class
+
+// one-to-many relationship
+using Lab3;
+
+Course Software = new Course(200, "Software Developer", 30);
+Student Jimmy = new Student(1000, "Jimmy", "Smith");
+
+HashSet<Enrolment> enrolments = new HashSet<Enrolment>();
+
+// a course can have many students in it
+// and a student can take one course
+// one-to-many relationship (one course, many students)
+
+// "one" component needs a collection of the many (course needs a collection of students)
+// "many" component needs a property for the "one" (student needs a property of Course)
+
+// something outside of the objects creating the relationship between them
+
+try
+{
+    RegisterStudent(Jimmy, Software);
+    //DeregisterStudent(Jimmy, Software);
+    //Console.WriteLine(Jimmy.Course.Title);
+} catch (Exception ex)
+{
+    Console.WriteLine(ex.Message.ToString());
+}
+
+
+void RegisterStudent(Student student, Course course)
+{
+    Enrolment newEnrolment = new Enrolment(student, course);
+    student.CurrentEnrolment = newEnrolment;
+    course.AddEnrolment(newEnrolment);
+    enrolments.Add(newEnrolment);
+}
+void DeregisterStudent(Student student, Course course, Enrolment enrolment)
+{
+    student.CurrentEnrolment = null;
+    course.RemoveEnrolment(enrolment);
+}
---
+++ Lab3/Student.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Student
+    {
+        private int? _courseGrade;
+        public int? CourseGrade { get { return _courseGrade; } }
+        private DateTime? _registrationDate;
+        public DateTime? RegistrationDate
+        {
+            get { return _registrationDate; }
+            set
+            {
+                _registrationDate = value;
+            }
+        }
+
+        public void UnRegister()
+        {
+            _registrationDate = null;
+        }
+
+        public Enrolment CurrentEnrolment { get; set; }
+
+        public void SetCourseGrade(int grade)
+        {
+            if (Course == null)
+            {
+                throw new Exception("Not enrolled in a course");
+            }
+            else if (grade < 0 || grade > 100)
+            {
+                throw new Exception("Grade must be between 0 and 100");
+            }
+            else
+            {
+                _courseGrade = grade;
+            }
+        }
+        public void RemoveGrade()
+        {
+            _courseGrade = null;
+        }
+        private int _studentId;
+        public int StudentId { get { return _studentId; } }
+        private void _setStudentId(int studentId)
+        {
+            if (studentId > 0)
+            {
+                _studentId = studentId;
+            }
+            else
+            {
+                throw new Exception("Student ID must be greater than zero.");
+            }
+        }
+
+
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    _firstName = value;
+                }
+                else
+                {
+                    throw new Exception("Value cannot be empty.");
+                }
+            }
+        }
+
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    _lastName = value;
+                }
+                else
+                {
+                    throw new Exception("Value cannot be empty.");
+                }
+            }
+        }
+
+        // many students can each take one course
+        public Course Course { get; set; }
+
+
+        // === CONSTRUCTORS ===
+        public Student(int studentId)
+        {
+            _setStudentId(studentId);
+        }
+
+        public Student(int studentId, string firstName, string lastName)
+        {
+            _setStudentId(studentId);
+
+            // if we define public set methods on properties, we should ONLY use those to change the value of a field, so that we use their validation
+            FirstName = firstName;
+            LastName = lastName;
+
+            // if we use a field to change a value, we do not validate
+            // avoid direct assignment to a field (e.g. _lastName = lastName)
+        }
+    }
+}
